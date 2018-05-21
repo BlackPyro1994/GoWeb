dcc3fe85692b7ba65bc11843875d5a72274ffaff
PeBoddy <pascal.decker@outlook.com>
2018-05-21 10:35:05 +0200

    ALLES FEDDIG!!

--- server.go
+++ server.go
@@ -3,11 +3,44 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"html/template"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+
+	data := TodoPageData{
+		PageTitle: "My TODO list",
+		Todos: []Todo{
+			{Title: "Task 1", Done: false},
+			{Title: "Task 2", Done: true},
+			{Title: "Task 3", Done: true},
+		},
+	}
+
+	tmpl := template.Must(template.ParseFiles("test.tmpl"))
+	tmpl.Execute(w, data)
+}
+func world(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "WORLD")
+}
+
 func main() {
+	server := http.Server{
+		Addr: ":80",
+	}
 
-	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/world", world)
 
-	http.ListenAndServe(":80", nil)
+	server.ListenAndServe()
 }
